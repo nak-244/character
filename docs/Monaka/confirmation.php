724bd451efeed64b58d65872491100e2d73c339b
nak-244 <t09024656860@gmail.com>
2019-03-06 15:36:24 +0900

    up

--- docs/Monaka/confirmation.php
+++ docs/Monaka/confirmation.php
@@ -8,11 +8,10 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>メールフォーム</title>
+  <title>エントリー内容確認画面　【しごとら】イメージキャラクターデザイン</title>
   <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/common.css">
   <link rel="stylesheet" href="css/confirmation.css">
-  <link rel="stylesheet" href="flat.css">
   <!--[if lt IE 9]>
   <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
